1901555313ae80356dd4f298069faa5aac56a81b
njuku9947 <njukun2000@gmail.com>
2026-07-25 13:30:55 +0000

    Add security headers after OWASP ZAP testing

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.after_request
+def add_security_headers(response):
+    response.headers["Content-Security-Policy"] = "default-src 'self'"
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    return response
+
 @app.route("/")
 def home():
     return "Mobile Banking API Running"
